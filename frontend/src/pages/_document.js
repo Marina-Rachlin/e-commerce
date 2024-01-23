ee48f583af3a375c6c0b2577f387d92003752e68
Marina Rachlin <Marina.rachlin@gmail.com>
2024-01-23 22:37:50 +0200

    cors changes

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -12,7 +12,7 @@ export default function Document() {
         />
         <link rel="icon" href="/assets/img/sm-logo.svg" />
         <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"></link>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-xk1XoiRD6fnFuLftdIJ6tbjLh/+q0w2q3CZ5hFtC5xuCO/5bIItuKPommeIC8DW2g6zgYG7z4yv8LtaBz6lAag==" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         
 
       </Head>
