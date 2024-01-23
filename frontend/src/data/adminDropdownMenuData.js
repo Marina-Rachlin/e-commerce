8351e87bd1f0573bb47ea269225a8617241c6c97
Marina Rachlin <Marina.rachlin@gmail.com>
2024-01-23 16:36:01 +0200

    data dir included

---
+++ frontend/src/data/adminDropdownMenuData.js
@@ -0,0 +1,23 @@
+export const adminDropdownMenuData = [
+    {
+      id: 1,
+      name: "Dashboard",
+      icon: "la-home",
+      routePath: "/admin",
+      active: "active",
+    },
+    {
+      id: 2,
+      name: "My Account",
+      icon: "la-user-tie",
+      routePath: "/my-account",
+      active: "",
+    },
+    {
+      id: 11,
+      name: "Logout",
+      icon: "la-sign-out",
+      routePath: "/login",
+      active: "",
+    },
+  ];
